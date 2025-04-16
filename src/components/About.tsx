46eeede04f69fe05491d5dec1e1e37208b7c240e
Arthur Zanon de Andrade <zanondea@gmail.com>
2025-04-15 22:21:14 -0300

    Qery params

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,17 @@
-export default function About (){
-    return(
-        <h2>About</h2>
-    )
-};
+import { useSearchParams } from "react-router";
+
+function About (){
+    const [searchParams] = useSearchParams();
+    const term = searchParams.get('term');
+    const admin = searchParams.get('admin');
+    const consulta= searchParams.get('consulta');
+
+    return (<>
+        <div>Termo de busca: {term}</div>;
+        <div>Termo de admin {admin}</div>;
+        <div>Consulta {consulta}</div>;
+        </>);
+}
+
+
+export default About;
