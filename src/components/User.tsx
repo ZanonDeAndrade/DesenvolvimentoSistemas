ce9d2b2ec7f47ab3c0daac2e7973438699d6fc16
Arthur Zanon de Andrade <zanondea@gmail.com>
2025-04-15 22:10:57 -0300

    User

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,5 +1,28 @@
-export default function User (){
+import { useParams } from "react-router";
+import { useState } from "react";
+import { useEffect } from "react";
+
+
+export default function User(){
+    const {id} = useParams();
+
+     const [ user, setUser ] = useState <any>();
+    
+        const fetchUser = async () => {
+            const response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+            const userData = await response.json();
+            setUser(userData);
+        }
+    
+        useEffect(() => {
+            fetchUser();
+        }, [])
+
     return(
-        <h2>User</h2>
+        <div>
+            <h2>User</h2>
+            <p>{user && user.id}</p>
+            <p>{user && user.name}</p>
+        </div>
     )
-};
+}
