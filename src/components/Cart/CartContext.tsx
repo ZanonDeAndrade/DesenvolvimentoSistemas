9db2e9318bddc9d4e6ef3193276351112efdad9e
Arthur Zanon <zanondea@gmail.com>
2025-06-01 16:41:42 -0300

    Erro na importação do useNavigate arrumado

--- src/components/Cart/CartContext.tsx
+++ src/components/Cart/CartContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useReducer } from 'react';
+import { createContext, useContext, useReducer } from 'react';
 import type { ReactNode } from 'react';
 import type { SushiItem } from '../Types/index';
 
